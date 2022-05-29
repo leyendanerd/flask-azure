ca3e1bb4decba76b70b44386bac646b39a582a20
leyendanerd <leyendanerd@gmail.com>
2022-05-29 19:10:08 -0400

    fixes

--- src/models/contacts.py
+++ src/models/contacts.py
@@ -2,10 +2,10 @@ from utils.db import db
 
 
 class Contact(db.Model):
-    id = db.Colum(db.Integer, primary_key=True)
-    fullname = db.Colum(db.String(80))
-    email = db.Colum(db.String(100))
-    phone = db.Colum(db.String(100))
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(80))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
 
     def __init__(self, fullname, email, phone):
         self.fullname = fullname
