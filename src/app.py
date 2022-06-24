d1823173536eecb6a561d33903f092e3f03a2fce
leyendanerd <leyendanerd@gmail.com>
2022-06-24 15:44:16 -0400

    changes

--- src/app.py
+++ src/app.py
@@ -1,20 +1,9 @@
 import os
-import sentry_sdk
 from flask import Flask
 from routes.contacts import contacts
 from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_CONNECTION 
-import sentry_sdk
 from flask import Flask
-from sentry_sdk.integrations.flask import FlaskIntegration
-
-sentry_sdk.init(
-    dsn="https://0638fb48cd26422c8af88c268b0a8b85@o1275124.ingest.sentry.io/6523802",
-    integrations=[
-        FlaskIntegration(),
-    ],
-    traces_sample_rate=1.0
-)
 
 app = Flask(__name__)
 
