0d04d7b8c0a2b8a10203c8b978e84c23b6297715
leyendanerd <leyendanerd@gmail.com>
2022-06-27 22:48:32 -0400

    changes

--- src/models/contacts.py
+++ src/models/contacts.py
@@ -1,11 +1,9 @@
 from utils.db import db
-
-
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    phone = db.Column(db.String(100))
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    phone = db.Column(db.String(100), nullable=False, unique=True)
 
     def __init__(self, fullname, email, phone):
         self.fullname = fullname
